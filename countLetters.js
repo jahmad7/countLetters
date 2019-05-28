05ed53dff76573bae8c6687730ddc86c3e5cb97a
Junaid <jahmad095@gmail.com>
2019-05-28 18:32:32 -0400

    inventory of strings per entry

--- countLetters.js
+++ countLetters.js
@@ -1,1 +1,19 @@
-hello world 
+function countLetters(word) {
+    var letterInventory = {};
+
+    word = word.split(" ").join("").toLowerCase();
+
+    for (var char of word) {
+        if (letterInventory[char] === undefined){
+            letterInventory[char] = 1 ;
+        }
+        else {
+            var currentValue = letterInventory[char];
+            currentValue++
+            letterInventory[char] = currentValue;
+        }
+    }
+    return letterInventory;
+}
+
+console.log(countLetters("hello world"));
